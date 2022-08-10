c5b8f7956275df867c8269d13d28ba122ba72013
Alex <baculaa@oregonstate.edu>
2022-08-10 02:55:38 -0700

    adding base for local planner

--- track_and_plan/local_planner.py
+++ track_and_plan/local_planner.py
@@ -8,14 +8,24 @@ class LocalPlanner:
         pass
     def get_cmd_vel(self,current_pose,goal_pose):
         pass
+        ## This will make a list of the cmd vel commands to be sent to the robots
     def main(self,tracker,GlobalPlanner):
+        # Get global paths
+        GlobalPlanner.main(tracker, goals)
         paths = GlobalPlanner.all_paths
 
-        all_at_goals = [False,False,False]
+        # Get number of robots
+        num_robots = len(tracker._ROBOT_IDS)
+
+        # Set all robots to not be at the local goal
+        all_at_goals = []
+        for i in range(num_robots):
+            all_at_goals.append(False)
+        # Get a local
         i = 0
         goal_increment = 10
         while not all(all_at_goals):
-            for robot in len(tracker.ROBOT_IDS):
+            for robot in range(num_robots):
                 robot_id = tracker.ROBOT_IDS[robot]
                 robot_path = paths[robot]
                 current_pose = [int(tracker._robots[robot_id][0]),int(tracker._robots[robot_id][1])]
@@ -24,6 +34,7 @@ class LocalPlanner:
                     self.get_cmd_vel(current_pose,goal_pose)
                 else:
                     all_at_goals[robot] = True
+
 
 if __name__ == '__main__':
     # Init global planner
